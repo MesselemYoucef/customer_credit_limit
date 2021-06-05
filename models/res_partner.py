14e0063f67fc0d99e986bc8ad34ade7abb8dfa2f
YoucefMesselem <messelemy@gmail.com>
2021-06-05 16:13:15 +0400

    added a computed field to the res.partner ingerited model

--- models/res_partner.py
+++ models/res_partner.py
@@ -8,6 +8,14 @@ class ResPartner(models.Model):
 
     credit_limit = fields.Float(string="Credit Limit")
     amount_available = fields.Float(string="Amount Available")
-    total_receivable = fields.Float(string="Total Receivable", stored="False")
+    total_receivable = fields.Float(string="Total Receivable", stored="False", compute="_compute_try_to_upgrade")
+
     total_payable = fields.Float(string="Total Payable")
     balance = fields.Float(string="Balance")
+
+    @api.depends("balance", "name")
+    def _compute_try_to_upgrade(self):
+        self.total_receivable = self.balance
+        print(self.balance)
+        print("name --------->", self.name)
+
